e2b8e440ba49f1840037036e8c89f0b471888ef9
wizard <925553434@qq.com>
2022-11-13 14:54:50 +0800

    my custom update

---
+++ spring-research/src/main/java/com/spring/research/embed/tomcat/config/BeanA.java
@@ -0,0 +1,11 @@
+package com.spring.research.embed.tomcat.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BeanA {
+
+	public BeanA(){
+		System.out.println("BeanA");
+	}
+}
